9fa2c1519e56536ef9be10a4938a62de5985360b
Chaithanya Shaji <139004346+chaithanyashaji@users.noreply.github.com>
2024-07-31 17:59:11 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,78 @@
+import express from "express";
+import bodyParser from "body-parser";
+import pg from "pg";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+const port = 3000;
+
+const db = new pg.Client({
+  user: process.env.PG_USER,
+  host: process.env.PG_HOST,
+  database: process.env.PG_DATABASE,
+  password: process.env.PG_PASSWORD,
+  port: process.env.PG_PORT,
+});
+
+db.connect().catch(err => console.error("Error connecting to the database", err.stack));
+
+let quiz = [];
+
+const loadQuizData = async () => {
+  try {
+    const res = await db.query("SELECT * FROM capitals");
+    quiz = res.rows;
+  } catch (err) {
+    console.error("Error executing query", err.stack);
+  }
+};
+
+loadQuizData();
+
+let totalCorrect = 0;
+let currentQuestion = {};
+
+// Middleware
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+// GET home page
+app.get("/", async (req, res) => {
+  totalCorrect = 0;
+  await nextQuestion();
+  console.log(currentQuestion);
+  res.render("index.ejs", { question: currentQuestion });
+});
+
+// POST a new post
+app.post("/submit", (req, res) => {
+  let answer = req.body.answer.trim();
+  let isCorrect = false;
+  if (currentQuestion.capital.toLowerCase() === answer.toLowerCase()) {
+    totalCorrect++;
+    console.log(totalCorrect);
+    isCorrect = true;
+  }
+
+  nextQuestion();
+  res.render("index.ejs", {
+    question: currentQuestion,
+    wasCorrect: isCorrect,
+    totalScore: totalCorrect,
+  });
+});
+
+async function nextQuestion() {
+  if (quiz.length > 0) {
+    const randomCountry = quiz[Math.floor(Math.random() * quiz.length)];
+    currentQuestion = randomCountry;
+  } else {
+    currentQuestion = { country: "No data available", capital: "" };
+  }
+}
+
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
